ca1381d15c385392f18a76e8d064e634bedde407
Naoki Saruta <katsusand1@gmail.com>
2019-10-04 14:54:41 +0900

    Change directory where intermediate files are generated to src directory
    Run latexmk in src directory to generate intermediate files in src there
    Create directory where to locate src tex files

--- compile.sh
+++ compile.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-docker run --rm -it -v $PWD:/workdir latex-template:latest latexmk -pvc $1
+SRC_FILE=$(echo $1 | awk -F'[/]' '{printf "%s/%s", $2, $3}')
+docker run --rm -it -v $PWD:/workdir latex-template:latest /bin/ash -c "cd src && latexmk -pvc $SRC_FILE"
